419cc49ff8d19fc2c3ff8b0fcbea0cf1bcb1df8e
aangelzurita <zuritazs27@gmail.com>
2025-10-24 01:43:52 -0600

    Clean up ESLint disable comments
    Removed duplicate ESLint disable comments.

--- app/CalendarioDemo.tsx
+++ app/CalendarioDemo.tsx
@@ -1,6 +1,4 @@
 /* eslint-disable @typescript-eslint/no-explicit-any, react-hooks/exhaustive-deps */
-/* eslint-disable @typescript-eslint/no-explicit-any, react-hooks/exhaustive-deps */
-/* eslint-disable @typescript-eslint/no-explicit-any, react-hooks/exhaustive-deps */
 'use client';
 import { useMemo, useState } from "react";
 import { Card, CardContent } from "@/components/ui/card";
